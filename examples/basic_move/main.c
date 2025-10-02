9625559d3e86c51abac6fc609c7318bcf49e5813
rozenc <38523735+rozenc@users.noreply.github.com>
2025-10-02 12:51:11 +0300

    Update main.c

--- examples/basic_move/main.c
+++ examples/basic_move/main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include "esp_stepper.h"
 
+StepperMotor motor;
+
+void app_main(void) {
+    stepper_init(&motor, 27, 25, 26, 33);
+    stepper_set_max_speed(&motor, 1300);
+    stepper_set_acceleration(&motor, 1300);
+    stepper_enable(&motor);
+    stepper_set_target(&motor, 4000);
+    stepper_run(&motor);
+}
